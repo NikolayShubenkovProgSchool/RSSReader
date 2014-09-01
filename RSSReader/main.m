d8921ceda79e5c2a29062c8aac16c6ef8a465911
NikolayShubenkovProgSchool <nshubenkov@NShubenkovs-MacBook-Pro.local>
2014-09-02 02:32:52 +0400

    Initial Commit

---
+++ RSSReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSSReader
+//
+//  Created by n.shubenkov on 02/09/14.
+//  Copyright (c) 2014 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
